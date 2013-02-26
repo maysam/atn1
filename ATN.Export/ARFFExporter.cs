c42bac9f1b913f2fac4c5f202f81bc48e7ad643e
Zeb Sampedro <sampedro@colorado.edu>
2013-02-26 13:17:55 -0800

    Added stub for ARFF exporter.

---
+++ ATN.Export/ARFFExporter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace ATN.Export
+{
+    class ARFFExporter
+    {
+        public static void Export(SourceNode[] Nodes, SourceEdge[] Edges, Stream DestinationStream)
+        {
+            //This method needs to identify training data from the database, generate and export
+            //an ARFF file of training data for use by WEKA, and then generate the corresponding test data
+            //for making predictions.
+        }
+    }
+}
