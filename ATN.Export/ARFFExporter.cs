b8f9e93ebf798626d85eb0a32fedeffaa90f6974
Zeb Sampedro <sampedro@colorado.edu>
2013-03-03 17:51:13 -0800

    Added ARFF exporter for Train and Test data, and supporting methods in ATN.Data.Theories.

--- ATN.Export/ARFFExporter.cs
+++ ATN.Export/ARFFExporter.cs
@@ -3,16 +3,108 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.IO;
+using ATN.Data;
 
 namespace ATN.Export
 {
     class ARFFExporter
     {
-        public static void Export(SourceNode[] Nodes, SourceEdge[] Edges, Stream DestinationStream)
+        /// <summary>
+        /// Export nodes into an ARFF training data file for analysis in WEKA.
+        /// </summary>
+        /// <param name="Nodes">Nodes marked by RAs to train decision tree with</param>
+        /// <param name="DestinationStream">Output ARFF training data file</param>
+        public static void ExportTrain(SourceNode[] Nodes, Stream DestinationStream)
         {
-            //This method needs to identify training data from the database, generate and export
-            //an ARFF file of training data for use by WEKA, and then generate the corresponding test data
-            //for making predictions.
+            UnicodeEncoding UniEncoding = new UnicodeEncoding();
+            Theories Theories = new Theories();
+
+            // Write header to file
+            string ARFFHeader = "@RELATION atn\n";
+            ARFFHeader += "@ATTRIBUTE SourceId Numeric\n";
+            ARFFHeader += "@ATTRIBUTE EigenFactorValue Numeric\n";
+            ARFFHeader += "@ATTRIBUTE AttentionRatio Numeric\n";
+            ARFFHeader += "@ATTRIBUTE class {contributing, not-contributing}\n\n";
+            ARFFHeader += "@DATA\n\n";
+            DestinationStream.Write(UniEncoding.GetBytes(ARFFHeader),0,UniEncoding.GetByteCount(ARFFHeader));
+
+            //Write training data to file
+            string datum = null;
+            foreach (var Node in Nodes)
+            {
+                TheoryMembership TM = Theories.GetTheoryMembershipForSource(Node.SourceId);
+                TheoryMembershipSignificance TMS = Theories.GetTheoryMembershipSignificanceForSource(Node.SourceId);
+                datum = "";
+
+                //SourceId
+                datum += TM.SourceId.ToString();
+                datum += ", ";
+                //EigenFactorValue
+                datum += TM.EigenFactorValue.ToString();
+                datum += ", ";
+                //AttentionRatio
+                datum += TM.AttentionRatio.ToString();
+                datum += ", ";
+                //class
+                if ((bool)TMS.RAMarkedContributing)
+                {
+                    datum += "contributing\n";
+                }
+                else
+                {
+                    datum += "not-contributing\n";
+                }
+
+                DestinationStream.Write(UniEncoding.GetBytes(datum), 0, UniEncoding.GetByteCount(datum));
+            }
+
+            DestinationStream.Flush();
+            DestinationStream.Close();
+        }
+
+        /// <summary>
+        /// Export nodes into an ARFF test data file for J48 classification.
+        /// </summary>
+        /// <param name="Nodes">Unmarked nodes to be classified</param>
+        /// <param name="DestinationStream">Output ARFF test data file</param>
+        public static void ExportTest(SourceNode[] Nodes, Stream DestinationStream)
+        {
+            UnicodeEncoding UniEncoding = new UnicodeEncoding();
+            Theories Theories = new Theories();
+
+            // Write header to file
+            string ARFFHeader = "@RELATION atn-test\n";
+            ARFFHeader += "@ATTRIBUTE SourceId Numeric\n";
+            ARFFHeader += "@ATTRIBUTE EigenFactorValue Numeric\n";
+            ARFFHeader += "@ATTRIBUTE AttentionRatio Numeric\n";
+            ARFFHeader += "@ATTRIBUTE class {contributing, not-contributing}\n\n";
+            ARFFHeader += "@DATA\n\n";
+            DestinationStream.Write(UniEncoding.GetBytes(ARFFHeader), 0, UniEncoding.GetByteCount(ARFFHeader));
+
+            //Write training data to file
+            string datum = null;
+            foreach (var Node in Nodes)
+            {
+                TheoryMembership TM = Theories.GetTheoryMembershipForSource(Node.SourceId);
+                datum = "";
+
+                //SourceId
+                datum += TM.SourceId.ToString();
+                datum += ", ";
+                //EigenFactorValue
+                datum += TM.EigenFactorValue.ToString();
+                datum += ", ";
+                //AttentionRatio
+                datum += TM.AttentionRatio.ToString();
+                datum += ", ";
+                //class
+                datum += "?\n";
+
+                DestinationStream.Write(UniEncoding.GetBytes(datum), 0, UniEncoding.GetByteCount(datum));
+            }
+
+            DestinationStream.Flush();
+            DestinationStream.Close();
         }
     }
 }
