ed5f5359ba3430311c545f76fb43b0ee7db5fa5a
Deema <deema.alomar@colorado.edu>
2013-03-21 03:22:42 -0600

    launcher page updated

--- ATN.Web/launcher.aspx.designer.cs
+++ ATN.Web/launcher.aspx.designer.cs
@@ -58,6 +58,24 @@ namespace ATN.Web {
         protected global::System.Web.UI.WebControls.GridView DataSourceGrid;
         
         /// <summary>
+        /// recrawl control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.CheckBox recrawl;
+        
+        /// <summary>
+        /// crawlperiod control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList crawlperiod;
+        
+        /// <summary>
         /// btnSubmit control.
         /// </summary>
         /// <remarks>
